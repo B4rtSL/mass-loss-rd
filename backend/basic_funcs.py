7ac2295b20eeb19b410276df54e43c6637837873
B4rtSL <bartsl1029@gmail.com>
2023-02-26 17:41:07 +0100

    feat: basic_funcs development

--- backend/basic_funcs.py
+++ backend/basic_funcs.py
@@ -20,35 +20,51 @@ def aero_prep(path):
     list_alpha = data['alpha-plane'].values.tolist()
     array_alpha = np.array(list_alpha)
 
+    #finding of max and min values indexes
+    for i in range(len(array_cz)):
+        if array_cz[i] == max(array_cz):
+            max_value_index = i
+
+    for j in range(len(array_cz)):
+        if array_cz[j] == min(array_cz):
+            min_value_index = j
+
+    #cutting original input arrays to needed range of values for better fit
+    new_cz_arr = array_cz[min_value_index:max_value_index+1]
+    new_alpha_arr = array_alpha[min_value_index:max_value_index+1]
+
     # 1st column - alpha degree, 2nd column cz coeff.
-    double_arr = [array_alpha, array_cz]
+    double_arr = [new_alpha_arr, new_cz_arr]
 
     return double_arr
 
-def interpolator(double_arr):
-    coeff_arr = np.polyfit(double_arr[0], double_arr[1], 6)
-    reverse_coeff_arr = coeff_arr[::-1]
-    polies = []
-    for i in double_arr[0]:
-        for j in range(7):
-            pom = i**j*reverse_coeff_arr[j]
-            poly = poly + pom 
-        polies.append(poly)
-    polies_arr=np.array(polies)
-    return print(polies_arr)
-
-
-def cx(aero_prep, cx0, lambda_e):
-    path = input()
-    double_arr = aero_prep(path)
+#this function prepares drag-polar array based on cz-charasteristics and user input
+def cx(aero_prep_res, cx0, lambda_e):
+    double_arr = aero_prep_res
     cz = double_arr[1]
     pi=np.pi
     cx  = []
     for i in cz:
         cxs=cx0+i*i/pi/lambda_e
         cx.append(cxs)
-
     array_cx = np.array(cx)
     return array_cx
+
+#this function takes users input file consisting of cz/alpha points and returns array of interpolated values
+#args are results of aero-prep and degree of wanted polynomial
+def interpolator(double_arr, deg, new_arg_range_arr):
+    coeff_arr = np.polyfit(double_arr[0], double_arr[1], deg)
+    reverse_coeff_arr = coeff_arr[::-1]
+    polies = []
+    for i in new_arg_range_arr:
+        pom = 0
+        poly = 0
+        for j in range(deg+1):
+            pom = reverse_coeff_arr[j]*i**j
+            poly = poly + pom 
+        polies.append(poly)
+    polies_arr=np.array(polies)
+    return polies_arr
+
 
 #def lift_to_drag(aero_prep, cx):
--- backend/tests.py
+++ backend/tests.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import interpolate
 import basic_funcs
+import matplotlib.pyplot as plt
 
 # 'C:/Users/barto/Desktop/inżynierka/test-data/cz-data.xlsx'
 test2 = input()
@@ -31,17 +32,49 @@ def aero_prep(path):
     
   
 foo = aero_prep(test2)
-print(foo[1])
-coeff_arr = np.polyfit(foo[0], foo[1], 6)
+
+cz_arr = foo[1]
+alph_arr = foo[0]
+print(max(cz_arr))
+print(min(cz_arr))
+print(len(cz_arr))
+
+for i in range(len(cz_arr)):
+    if cz_arr[i] == max(cz_arr):
+        max_value_index = i
+
+for j in range(len(cz_arr)):
+    if cz_arr[j] == min(cz_arr):
+        min_value_index = j
+
+print(max_value_index)
+print(min_value_index)
+new_cz_arr = cz_arr[min_value_index:max_value_index+1]
+print(new_cz_arr)
+new_alpha_arr = alph_arr[min_value_index:max_value_index+1]
+
+coeff_arr = np.polyfit(new_alpha_arr, new_cz_arr,5)
 reverse_coeff_arr = coeff_arr[::-1]
-print(coeff_arr)
-print(reverse_coeff_arr)
+#print(coeff_arr)
+#print(reverse_coeff_arr)
 polies = []
-poly = 0
-for i in foo[0]:
-    for j in range(7):
-        pom = (i**j)*(reverse_coeff_arr[j])
+"""
+for i in new_alpha_arr:
+    poly = i**0*reverse_coeff_arr[0] + i**1*reverse_coeff_arr[1] + i**2*reverse_coeff_arr[2] + i**3*reverse_coeff_arr[3] + i**4*reverse_coeff_arr[4] + i**5*reverse_coeff_arr[5] 
+    polies.append(poly)
+polies_arr=np.array(polies)
+"""
+
+for i in new_alpha_arr:
+    pom = 0
+    poly = 0
+    for j in range(6):
+        pom = reverse_coeff_arr[j]*i**j
         poly = poly + pom 
     polies.append(poly)
 polies_arr=np.array(polies)
 print(polies_arr)
+
+fig = plt.figure()
+plt.plot(foo[0],foo[1], new_alpha_arr, polies_arr, new_alpha_arr, new_cz_arr)
+plt.show()
