1e1600d0fe66847b7ffc49ae25dd30340d434161
B4rtSL <bartsl1029@gmail.com>
2023-03-01 21:55:50 +0100

    feat: update of main script to match basic_funcs

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from breguet_simple_jet import breguetJet
-from breguet_simple_propeller import breguetPropeller
 from pydantic import BaseModel
+import breguet_simple_jet as bsj
+import breguet_simple_propeller as bsp
 
 app = FastAPI()
 
@@ -34,32 +34,84 @@ app.add_middleware(
 @app.post("/")
 def home(object: MyForm):
     
-    returned_dictProp=breguetPropeller(object.startmass,object.nompow,object.fuelcons,object.propnumber,object.altitude,object.aspectratio,object.cx0,object.area,object.vmin,object.vmax,object.efficiency)
-    returned_dictJet=breguetJet(object.startmass,object.nompow,object.fuelcons,object.propnumber,object.altitude,object.aspectratio,object.cx0,object.area,object.vmin,object.vmax)
+    returned_dictProp1 = bsp.breguetPropeller(object.startmass, object.nompow, object.fuelcons, object.propnumber, object.altitude, object.aspectratio, object.cx0, object.area, object.vmin, object.vmax, object.efficiency)
+    returned_dictJet1 = bsj.breguetJet(object.startmass,object.nompow,object.fuelcons,object.propnumber,object.altitude,object.aspectratio,object.cx0,object.area,object.vmin,object.vmax)
+    returned_dictProp2 = bsp.breguetPropeller_2set(object.startmass, object.nompow, object.fuelcons, object.propnumber, object.altitude, object.aspectratio, object.cx0, object.area, object.vmin, object.vmax, object.efficiency)
+    returned_dictJet2 = bsj.breguetJet_2set(object.startmass,object.nompow,object.fuelcons,object.propnumber,object.altitude,object.aspectratio,object.cx0,object.area,object.vmin,object.vmax)
+    returned_dictProp3 = bsp.breguetPropeller_3set(object.startmass, object.nompow, object.fuelcons, object.propnumber, object.altitude, object.aspectratio, object.cx0, object.area, object.vmin, object.vmax, object.efficiency)
+    returned_dictJet3 = bsj.breguetJet_3set(object.startmass,object.nompow,object.fuelcons,object.propnumber,object.altitude,object.aspectratio,object.cx0,object.area,object.vmin,object.vmax)
     if object.proptype == 'propeller':
         return [{
-            'x': returned_dictProp['x_list'],
-            'y' : returned_dictProp['times_list'],
+            'x': returned_dictProp1['x_list'],
+            'y' : returned_dictProp1['times_list'],
             'type': 'line',
             'name': '100*T [h]'
         },{
-            'x': returned_dictProp['x_list'],
-            'y' : returned_dictProp['ranges_list'],
+            'x': returned_dictProp1['x_list'],
+            'y' : returned_dictProp1['ranges_list'],
             'type': 'line',
             'name': 'L [km]'
         }]
-    else:
+    elif object.proptype == 'propeller-set-2':
         return [{
-            'x': returned_dictJet['x_list'],
-            'y' : returned_dictJet['times_list'],
+            'x': returned_dictProp2['x_list'],
+            'y' : returned_dictProp2['times_list'],
             'type': 'line',
             'name': '100*T [h]'
         },{
-            'x': returned_dictJet['x_list'],
-            'y' : returned_dictJet['ranges_list'],
+            'x': returned_dictProp2['x_list'],
+            'y' : returned_dictProp2['ranges_list'],
             'type': 'line',
             'name': 'L [km]'
         }]
+    elif object.proptype == 'propeller-set-3':
+        return [{
+            'x': returned_dictProp3['x_list'],
+            'y' : returned_dictProp3['times_list'],
+            'type': 'line',
+            'name': '100*T [h]'
+        },{
+            'x': returned_dictProp3['x_list'],
+            'y' : returned_dictProp3['ranges_list'],
+            'type': 'line',
+            'name': 'L [km]'
+        }]
+    elif object.proptype == 'jet':
+        return [{
+            'x': returned_dictJet1['x_list'],
+            'y' : returned_dictJet1['times_list'],
+            'type': 'line',
+            'name': '100*T [h]'
+        },{
+            'x': returned_dictJet1['x_list'],
+            'y' : returned_dictJet1['ranges_list'],
+            'type': 'line',
+            'name': 'L [km]'
+        }]
+    elif object.proptype == 'jet-set-2':
+        return [{
+            'x': returned_dictJet2['x_list'],
+            'y' : returned_dictJet2['times_list'],
+            'type': 'line',
+            'name': '100*T [h]'
+        },{
+            'x': returned_dictJet2['x_list'],
+            'y' : returned_dictJet2['ranges_list'],
+            'type': 'line',
+            'name': 'L [km]'
+        }]
+    elif object.proptype == 'jet-set-3':
+        return [{
+            'x': returned_dictJet3['x_list'],
+            'y' : returned_dictJet3['times_list'],
+            'type': 'line',
+            'name': '100*T [h]'
+        },{
+            'x': returned_dictJet3['x_list'],
+            'y' : returned_dictJet3['ranges_list'],
+            'type': 'line',
+            'name': 'L [km]'
+        }]    
     
 
 
