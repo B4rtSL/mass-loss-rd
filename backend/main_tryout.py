06136eb52b8d7f1038984261f39fb8b75fb86ef4
B4rtSL <bartsl1029@gmail.com>
2023-04-17 00:10:30 +0200

    feat: time step tryouts with mpe and delta

--- backend/main_tryout.py
+++ backend/main_tryout.py
@@ -1,61 +1,13 @@
-import basic_funcs as basf
+from advanced_alg import advanced_alg
+from data_container import Cessna150
 import numpy as np
-import matplotlib.pyplot as plt
-import breguet_simple_jet as bsj
-import breguet_simple_propeller as bsp
+import pandas as pd
 
-altitude = 0
-area = 17.02
-aspectratio = 8.7
-cx0 = 0.0255
-efficiency = 0.8
-fuelcons = 0.23
-nompow = 290
-propnumber =2
-proptype = 'propeller'
-startmass = 2200
-vmax = 150
-vmin = 35
+cz_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-cz-data.xlsx"
+fuelcons_input_cessna = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-fuelcons-load-data.xlsx"
+rpm_input_cessna = 'C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-rpm-load-data.xlsx'
+engine_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-power-const.xlsx"
+eta_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/eta-velo.xlsx"
 
-breguet1 = bsp.breguetPropeller(startmass, nompow, fuelcons, propnumber, altitude, aspectratio, cx0, area, vmin, vmax, efficiency)
-breguet1_velocity = breguet1[0]
-breguet1_times = breguet1[1]
-breguet1_ranges = breguet1[2]
-
-breguet2 = bsp.breguetPropeller_2set(startmass, nompow, fuelcons, propnumber, altitude, aspectratio, cx0, area, vmin, vmax, efficiency)
-breguet2_velocity = breguet2[0]
-breguet2_times = breguet2[1]
-breguet2_ranges = breguet2[2]
-
-breguet3 = bsp.breguetPropeller_3set(startmass, nompow, fuelcons, propnumber, altitude, aspectratio, cx0, area, vmin, vmax, efficiency)
-breguet3_velocity = breguet3[0]
-breguet3_times = breguet3[1]
-breguet3_ranges = breguet3[2]
-
-'''
-fig = plt.figure()
-plt.plot(breguet1_velocity, breguet1_times, breguet1_velocity, breguet1_ranges)
-fig2 = plt.figure()
-plt.plot(breguet2_velocity, breguet2_times, breguet2_velocity, breguet2_ranges)
-fig3 = plt.figure()
-plt.plot(breguet3_velocity, breguet3_times, breguet3_velocity, breguet3_ranges)
-'''
-
-fig4 = plt.figure()
-plt.plot(breguet1_velocity, breguet1_ranges, "--r" , label = "Breguet-range-propeller-1")
-plt.plot(breguet2_velocity, breguet2_ranges,"-b" , label = "Breguet-range-propeller-2")
-plt.plot(breguet3_velocity, breguet3_ranges, "-.g" , label = "Breguet-range-propeller-3")
-plt.legend(loc="best")
-plt.xlabel("Velocity [km/h]")
-plt.ylabel("Range [km]")
-plt.title("Range on Velocity")
-
-fig5 = plt.figure()
-plt.plot(breguet1_velocity, breguet1_times, "--r" , label = "Breguet-time-propeller-1")
-plt.plot(breguet2_velocity, breguet2_times,"-b" , label = "Breguet-time-propeller-2")
-plt.plot(breguet3_velocity, breguet3_times, "-.g" , label = "Breguet-time-propeller-3")
-plt.legend(loc="best")
-plt.xlabel("Velocity [km/h]")
-plt.ylabel("Time [100h]")
-plt.title("Endurance on Velocity")
-plt.show()
+results05, time05, cpu05 = advanced_alg(rpm_input_cessna, fuelcons_input_cessna, eta_input, Cessna150, 0, 15)
+print('Krok 15s:', time05, 'CPU:', cpu05)
--- backend/time_step_tryouts.py
+++ backend/time_step_tryouts.py
@@ -1,5 +1,7 @@
 from advanced_alg import advanced_alg
 from data_container import Cessna150
+import numpy as np
+import pandas as pd
 
 cz_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-cz-data.xlsx"
 fuelcons_input_cessna = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-fuelcons-load-data.xlsx"
@@ -7,6 +9,8 @@ rpm_input_cessna = 'C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/ces
 engine_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/cessna-power-const.xlsx"
 eta_input = "C:/Users/barto/Desktop/inżynierka/test-data/cessna-data/eta-velo.xlsx"
 
+results05, time05, cpu05 = advanced_alg(rpm_input_cessna, fuelcons_input_cessna, eta_input, Cessna150, 0, 0.5)
+print('Krok 1s:', time05, 'CPU:', cpu05)
 
 results1, time1, cpu1 = advanced_alg(rpm_input_cessna, fuelcons_input_cessna, eta_input, Cessna150, 0, 1)
 print('Krok 1s:', time1, 'CPU:', cpu1)
@@ -23,7 +27,38 @@ print('Krok 15s:', time15, 'CPU:', cpu15)
 results30, time30, cpu30 = advanced_alg(rpm_input_cessna, fuelcons_input_cessna, eta_input, Cessna150, 0, 30)
 print('Krok 30s:', time30, 'CPU:', cpu30)
 
-
 results60, time60, cpu60 = advanced_alg(rpm_input_cessna, fuelcons_input_cessna, eta_input, Cessna150, 0, 60)
 print('Krok 60s:', time60, 'CPU:', cpu60)
 
+array_Wtimes = [time05, time1, time5, time10, time15, time30, time60]
+array_CPUtimes = [cpu05, cpu1, cpu5, cpu10, cpu15, cpu30, cpu60]
+
+latex_Wtimes = np.around(array_Wtimes, decimals=5)
+latex_CPUtimes = np.around(array_CPUtimes, decimals=5)
+
+raw_data = {'Wall time [s]': latex_Wtimes,
+            'CPU time [s]': latex_CPUtimes
+            }
+
+df = pd.DataFrame(raw_data, columns=['Wall time [s]', 'CPU time [s]'])
+df.to_csv('C:/Users/barto/Desktop/inżynierka/test-data/test-results/results-time-step.csv')
+
+array_range_results = [results05[0], results1[0], results5[0], results10[0], results15[0], results30[0], results60[0]]
+array_endurance_results = [results05[1], results1[1], results5[1], results10[1], results15[1], results30[1], results60[1]]
+
+error_list = []
+for result in array_range_results:
+    error = np.divide(abs(np.subtract(results05[0], result)), results05[0])
+    error_list.append(error)
+
+mpe_list = []
+for array in error_list:
+    summed = np.sum(array)
+    mpe = 100 * summed / len(array)
+    mpe_list.append(mpe)
+
+latex_mpes = np.around(mpe_list, decimals=5)
+
+mpes_data_raw = {'MPE [%]': latex_mpes} 
+df2 = pd.DataFrame(mpes_data_raw, columns=['MPE [%]'])
+df2.to_csv('C:/Users/barto/Desktop/inżynierka/test-data/test-results/results-mpe.csv')
